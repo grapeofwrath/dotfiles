07cef277ba6a12e759900a8b6b677b73ca6bb512
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2024-04-06 11:38:36 -0500

    readme documentation

--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -1,12 +1,9 @@
 {lib,...}: with lib;
 let
-  # Recursively constructs an attrset of a dir, value of attrs is the filetype
   getDir = dir: mapAttrs
     (file: type: if type == "directory" then getDir "${dir}/${file}" else type)
     (builtins.readDir dir);
-  # Collects all files of a dir as list of strs of paths
   files = dir: collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
-  # Filters out dirs != *.nix, makes the strs absolute
   validFiles = dir: map
     (file: ./. + "/${file}")
     (filter
