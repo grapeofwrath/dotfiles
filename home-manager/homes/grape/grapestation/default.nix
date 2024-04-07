b78eedda22d60ebb21abf4e5e25778c3e602ec22
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2024-04-07 07:57:04 -0500

    added nushell to grapestation

--- home-manager/homes/grape/grapestation/default.nix
+++ home-manager/homes/grape/grapestation/default.nix
@@ -17,5 +17,13 @@
     # gaming
     wineWowPackages.staging
   ];
+
+  orion = {
+    shell = {
+      nushell.enable = true;
+    };
+  };
+
+  # Believe it or not, if you change this? Straight to jail.
   home.stateVersion = "23.11";
 }
