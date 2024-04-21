840470718cd831d8bbba3ca4c538112faf9c8e30
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2024-04-21 16:03:58 -0500

    Nixvim: added comment.nvim, enabled hidden files be default in oil, enabled file browser ext in telescope

--- home-manager/modules/nixvim/plugins/telescope.nix
+++ home-manager/modules/nixvim/plugins/telescope.nix
@@ -10,11 +10,18 @@
           "^target/"
         ];
       };
+      extensions.file-browser = {
+        enable = true;
+        settings.hidden = {
+          file_browser = true;
+          folder_browser = true;
+        };
+      };
     };
     keymaps = [
       {mode = "n"; key = "<leader>ff"; action = "<cmd>lua require('telescope.builtin').find_files({hidden = true})<CR>"; options.desc = "Find Files";}
       {mode = "n"; key = "<leader>fg"; action = "<cmd>lua require('telescope.builtin').live_grep({hidden = true})<CR>"; options.desc = "Grep Files";}
-      {mode = "n"; key = "<leader>fb"; action = "<cmd>lua require('telescope.builtin').buffers()<CR>"; options.desc = "Find Buffer";}
+      {mode = "n"; key = "<leader>fb"; action = "<cmd>Telescope file_browser path=%:p:h select_buffer=true<CR>"; options.desc = "File Browser";}
       {mode = "n"; key = "<leader>fh"; action = "<cmd>lua require('telescope.builtin').help_tags()<CR>"; options.desc = "Find Help";}
       {mode = "n"; key = "<leader>fd"; action = "<cmd>lua require('telescope.builtin').diagnostics()<CR>"; options.desc = "Find Diagnostics";}
       {mode = "n"; key = "<leader>ft"; action = "<cmd>lua require('telescope.builtin').treesitter()<CR>"; options.desc = "Find Treesitter";}
